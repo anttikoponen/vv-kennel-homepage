f6bd1587a4f5322c18d40791dbc142ee9ca81936
anttikoponen <anttikoponen@hotmail.fi>
2023-10-17 06:07:37 +0300

    debloy error fix on eslint with mapping key

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,24 @@
-import Image from "next/image"
-import fs from "fs"
-import Link from "next/link"
+import Image from "next/image";
+import fs from "fs";
+import Link from "next/link";
 
 const getPostMetadata = () => {
-  const folder = "posts/"
-  const files = fs.readdirSync(folder)
-  const markdownPosts = files.filter((file) => file.endsWith(".md"))
-  const slugs = markdownPosts.map((file) => file.replace(".md", ""))
-  return slugs
-}
+  const folder = "posts/";
+  const files = fs.readdirSync(folder);
+  const markdownPosts = files.filter((file) => file.endsWith(".md"));
+  const slugs = markdownPosts.map((file) => file.replace(".md", ""));
+  return slugs;
+};
 
 export default function Home() {
-  const postMetadata = getPostMetadata()
+  const postMetadata = getPostMetadata();
   const postPreviews = postMetadata.map((slug) => (
-    <div>
+    <div key="slugData">
       <Link href={`/posts/${slug}`}>
-        <h2>{slug}</h2>
+        <h2>Post: {slug}</h2>
       </Link>
     </div>
-  ))
+  ));
 
   return (
     <div>
@@ -40,5 +40,5 @@ export default function Home() {
         </div>
       </div>
     </div> */
-  )
+  );
 }
